28330063630e80923ffc992aa69e68030866eed4
Vinicius Isola <viniciusisola@gmail.com>
2019-05-04 17:11:55 -0400

    Add chapter 01 Hello Process sample

--- java/chapter-01/hello_process/src/main/java/org/visola/howthewebworks/serverside/Main.java
+++ java/chapter-01/hello_process/src/main/java/org/visola/howthewebworks/serverside/Main.java
@@ -1,14 +1,22 @@
 package org.visola.howthewebworks.serverside;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
-    public static void main (String [] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("What's your name? ");
-            String name = scanner.nextLine();
-            System.out.println("Hello " + name + "!");
+    public static void main (String [] args) throws Exception {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Process is shutting down...");
+            }
+        });
+
+        Random random = new Random();
+        while (true) { // Infinite loop
+            int next = random.nextInt(1000);
+            System.out.printf("Next random number was: %d\n", next);
+            Thread.sleep(1000);
         }
     }
 
