1596ae0458dad7b9dc2b30861f4239eee7b8a2e2
Vinicius Isola <viniciusisola@gmail.com>
2019-06-06 07:28:40 -0400

    Better handle the zipcodes found

--- java/chapter-01/zip_code_lookup/src/main/java/org/visola/howthewebworks/serverside/ZipCodeFinder.java
+++ java/chapter-01/zip_code_lookup/src/main/java/org/visola/howthewebworks/serverside/ZipCodeFinder.java
@@ -1,7 +1,11 @@
 package org.visola.howthewebworks.serverside;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.visola.howthewebworks.serverside.datareader.addressgroup.AddressGroup;
 
@@ -18,22 +22,44 @@ public class ZipCodeFinder {
     public void findLoop() {
         System.out.print("Type a number: ");
         String number = scanner.nextLine();
-        int recordNumber = 0;
-        for (AddressGroup address : addressGroups) {
-            if (address.getZipCode().contains(number)) {
-                recordNumber++;
-                System.out.printf("Found %s with zipcode %s\n", address.getStreet(), address.getZipCode());
-                if (recordNumber % 10 == 0) {
-                    System.out.printf("\nRecords printed so far: %d\n", recordNumber);
-                    System.out.print("- Press Enter to Continue or type S (or s) to leave > ");
-                    String input = scanner.nextLine();
-                    if ("S".equalsIgnoreCase(input)) {
-                        System.out.println();
-                        break;
-                    }
-                }
-            }
+
+        Map<String, List<AddressGroup>> groupsByZipCode = addressGroups.stream()
+            .filter(a -> a.getZipCode().contains(number))
+            .collect(Collectors.groupingBy(AddressGroup::getZipCode));
+
+        System.out.printf("Found %d zipcodes with %s%n", groupsByZipCode.size(), number);
+
+        if (groupsByZipCode.size() == 0) {
+            System.out.println();
+            return;
         }
+
+        Optional<String> maybeExplore = printAndPickZipCode(groupsByZipCode);
+        if (!maybeExplore.isPresent()) {
+            System.out.println();
+            return;
+        }
+
+        System.out.printf("Exploring %s%n%n", maybeExplore.get());
+    }
+
+    private Optional<String> printAndPickZipCode(Map<String, List<AddressGroup>> groupsByZipCode) {
+        int total = 0;
+        for (String zipCode : groupsByZipCode.keySet()) {
+            int countInZipCode = groupsByZipCode.get(zipCode).size();
+            total += countInZipCode;
+            System.out.printf("- %d address groups in %s%n", countInZipCode, zipCode);
+        }
+        System.out.printf("%d total address groups found%n%n", total);
+
+        System.out.println("Type a zipcode to explore or [Enter] to leave: ");
+        String zipCode = scanner.nextLine();
+
+        if (groupsByZipCode.containsKey(zipCode)) {
+            return Optional.of(zipCode);
+        }
+
+        return Optional.empty();
     }
 
 }
